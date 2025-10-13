7aefeba01b87e716392c9e88ea42878f184e0cdd
GabyNor05 <160027369+GabyNor05@users.noreply.github.com>
2025-10-13 15:00:52 +0200

    trying to deploy pt4
    1. Replaced Firebase with Cloudinary
    2. Cleared the Uploads file

--- backend/routes/progressNotesRoutes.js
+++ backend/routes/progressNotesRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const cloudinary = require('../config/cloudinary');
 const multer = require('multer');
 const upload = multer({ storage: multer.memoryStorage() });
+const pool = require('../config/db');
 
 router.post('/upload', upload.single('file'), async (req, res) => {
   try {
