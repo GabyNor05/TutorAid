e1e1b0c7ea899b873faa40b8e5f770b2a56ab3b0
GabyNor05 <160027369+GabyNor05@users.noreply.github.com>
2025-10-13 18:13:55 +0200

    trying to deploy pt6

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,15 +4,22 @@ require("dotenv").config();
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD || "", // leave empty if no password
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  ssl: {
+    // This is required to connect to Azure
+    rejectUnauthorized: true
+  }
 });
 
 pool.getConnection()
-  .then(() => console.log("✅ DB connected successfully!"))
+  .then(connection => {
+    console.log("✅ DB connected successfully!");
+    connection.release();
+  })
   .catch(err => console.error("DB connection error:", err));
 
 module.exports = pool;
